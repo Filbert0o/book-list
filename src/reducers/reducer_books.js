d1dac2ea0576e953603a22a9f3ea73be08a9b71f
Timothy Andrew Filbert <timfilbert97@gmail.com>
2018-01-14 18:29:57 -0500

    added book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+  return (
+    [
+      { title: 'Javascript: The Good Parts' },
+      { title: 'Trains on Rails' },
+      { title: 'Reactjs, The Main One' },
+      { title: 'Harry Potter: Chamber' }
+    ]
+  )
+}
