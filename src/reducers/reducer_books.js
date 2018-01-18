d30e0df333e3d6cbee6f6dee9c74c3477f807fb3
Timothy Andrew Filbert <timfilbert97@gmail.com>
2018-01-18 18:05:48 -0500

    added action creators

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 export default function() {
   return (
     [
-      { title: 'Javascript: The Good Parts' },
-      { title: 'Trains on Rails' },
-      { title: 'Reactjs, The Main One' },
-      { title: 'Harry Potter: Chamber' }
+      { title: 'Javascript: The Good Parts', pages: 101 },
+      { title: 'Trains on Rails', pages: 10000 },
+      { title: 'Reactjs, The Main One', pages: 231 },
+      { title: 'Harry Potter: Chamber', pages: 279 }
     ]
   )
 }
